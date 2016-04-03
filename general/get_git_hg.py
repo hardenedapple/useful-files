83e1e89584f7d9725ea8fe006dc58fabec64e4fc
hardenedapple <hardenedapple@server.fake>
2016-04-03 12:31:33 +0100

    git-remote-hg now has 'python2' in the shebang
    Don't need to manually change the shebang any more, so remove that from
    the script.

--- general/get_git_hg.py
+++ general/get_git_hg.py
@@ -3,6 +3,7 @@
 A script to download git-remote-hg.py to my PATH without the LICENSE and README
 
 Look at felipec/git for the git-remote-hg file
+
 """
 
 import urllib.request as urlreq
@@ -12,15 +13,7 @@ import re
 
 binpath = os.environ['HOME'] + '/bin/git-remote-hg'
 
-githgurl = 'https://raw.githubusercontent.com/felipec/git/master/git-remote-hg.py'
+githgurl = 'https://raw.githubusercontent.com/felipec/git-remote-hg/master/git-remote-hg'
 urlreq.urlretrieve(githgurl, binpath)
-
-with open(binpath, 'r') as oldfile:
-    lines = oldfile.readlines()
-    # Arch using 'python3' as 'python'
-    lines[0] = re.sub('#!/(.*)python', r'#!/\1python2', lines[0])
-
-with open(binpath, 'w') as newfile:
-    newfile.write(''.join(lines))
 
 os.chmod(binpath, stat.S_IXUSR|stat.S_IRUSR|stat.S_IWUSR)
